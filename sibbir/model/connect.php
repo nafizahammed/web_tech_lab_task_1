8e59afe4f6e43e0e3cb606a1d204f52492153af0
Md.Nafiz Ahammed <63756922+nafizahammed@users.noreply.github.com>
2022-08-06 14:03:45 +0600

    Update connect.php

--- sibbir/model/connect.php
+++ sibbir/model/connect.php
@@ -1,40 +1,21 @@
 <?php 
 
-		
+	function connect() {
 		$hostname = "localhost";
 		$username = "root";
 		$password = "";
-		$dbname = "book_shops";
+		$dbname = "online_books";
 
 		$conn = mysqli_connect($hostname, $username, $password, $dbname);
-
-		if (mysqli_connect_errno()){
-			echo "Connection Failed";
-			exit();
+        
+		if (! $conn) {
+			die("There were some problem in database connection.");
 		}
+		//else{
+        //     echo "connection successfully";
+        // }
+        
 
-		echo "Connection success";
-	
-	
-	
-?>
-
-
-
-<?php 
-	/*
-		
-		$hostname = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "book_shop";
-
-		$conn = new mysqli($hostname, $username, $password, $dbname);
-
-		if ($conn->connect_error) {
-			die("Connect failed.".$conn->connect_error);
-		}
-			//echo 'Connection Failed';
-			
-	*/	
+		return $conn;
+	}
 ?>
